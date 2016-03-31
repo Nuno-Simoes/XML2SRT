6cedbcd1df43218fbf09865ab48599bb926fda70
Nuno-Simoes <nunosimoes_86@hotmail.com>
2016-03-31 14:56:35 +0100

    removed folder

--- src/XML2SRT.java
+++ src/XML2SRT.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.File;
 import java.nio.file.Files;
@@ -7,7 +8,7 @@ import java.nio.file.Paths;
 
 public class XML2SRT {
 	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input;
@@ -15,24 +16,32 @@ public class XML2SRT {
 		System.out.println("To exit type exit");
 		System.out.println("Enter the directory where the file is:");
 		
-		while((input=br.readLine())!=null) {
-			if(input.equals("exit"))
-				return;
-			Files.walk(Paths.get(input)).forEach(filePath -> {
-				if (Files.isRegularFile(filePath) && filePath.toString().endsWith(".xml")) {
-					File inputFile = new File(filePath.toString());
-					XMLParser parser = new XMLParser();
-					SRTWriter writer = new SRTWriter();
-					Subtitle sub;
-					try {
-						sub = parser.parse(inputFile);
-						if(sub != null)
-							writer.write(sub, filePath.toString());
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		try {
+			while((input=br.readLine())!=null) {
+				if(input.equals("exit"))
+					return;
+				try {
+					Files.walk(Paths.get(input)).forEach(filePath -> {
+						if (Files.isRegularFile(filePath) && filePath.toString().endsWith(".xml")) {
+							File inputFile = new File(filePath.toString());
+							XMLParser parser = new XMLParser();
+							SRTWriter writer = new SRTWriter();
+							Subtitle sub;
+							try {
+								sub = parser.parse(inputFile);
+								if(sub != null)
+									writer.write(sub, filePath.toString());
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+				} catch (IOException e) {
+					System.out.println("No such file or directory");
 				}
-			});
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}	
 	}
 }
